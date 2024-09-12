a412a461b055c202b9d340dd77a7fe22673cad9f
sinan <sinan@SINAN-SENKUL>
2024-09-12 12:07:17 +0300

    Add project files.

---
+++ MVCProject/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com"
+            };
+            var order = new Order
+            {
+                Id=2,
+                ProductName="pizza",
+                Price=10,
+                Quantity=1,
+            };
+            var order2 = new Order
+            {
+                Id = 3,
+                ProductName = "milkshake",
+                Price = 5,
+                Quantity = 1,
+            };
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = {order, order2}
+            };
+            return View();
+        }
+    }
+}
---
+++ MVCProject/Models/CustomerOrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MVCProject.Models;
+
+namespace MVCProject.Models
+{
+    public class CustomerOrderViewModel
+    {
+        public Customer Customer { get; set; }
+        /*public Order Order { get; set; }*/
+        public List<Order> Orders { get; set;}
+    }
+}
